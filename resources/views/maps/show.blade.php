745bd2384d7874be83f8ae20789e59a0d23c2914
emad.mohamed.ivas <emad@ivas.com.eg>
2022-12-06 17:42:48 +0200

    emad,edit

--- resources/views/maps/show.blade.php
+++ resources/views/maps/show.blade.php
@@ -18,6 +18,7 @@
 
 
 
+<script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 
 <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
     integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
@@ -26,17 +27,61 @@
 
     <script>
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
-
-        const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-
-        const marker = L.marker([51.5, -0.09]).addTo(map)
-            .bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+    function addToMap(locations, center){
 
 
+/*
+        var map = L.map('map'),{
+                           center : center,
+                           zoom : 12
+                        });
+
+
+               L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    maxZoom: 19,
+                    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                }).addTo(map);
+*/
+
+
+
+                var map = L.map('map').setView(center, 13);
+
+                L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+
+              /*
+                L.marker([51.5, -0.09]).addTo(map)
+                    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                    .openPopup()
+*/
+
+
+
+     var customTitle ="";
+
+        for (var i= 0; i < locations.length; i++ ) {
+            customTitle = i.toString();
+                if (i == (locations. length - 1) ) {
+                     $customTitle = "last point" ;
+                }
+
+                marker = new L.marker([locations[i][0], locations[i][1]])
+                .addTo(map)
+                .bindPopup("test")
+                .openPopup() ;
+
+        }
+
+
+    }
+
+
+
+
+
+/*
 
         const popup = L.popup()
             .setLatLng([51.513, -0.09])
@@ -51,6 +96,26 @@
         }
 
         map.on('click', onMapClick);
+        */
+
+
+
+        $.ajax({
+            type:'GET',
+            url:'/tracking',
+            dataType : 'json' ,
+           // data:'_token = <?php echo csrf_token() ?>',
+            success:function(data) {
+                addToMap(data, data.slice(-1)[0])
+            } ,
+            error : function(request, error){
+           console.log(error) ;
+           console.log(JSON.stringify(request)) ;
+            }
+
+
+         });
+
 
     </script>
 
